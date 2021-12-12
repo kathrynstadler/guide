015651952e7290747ff09c2ae0481c43c588c72e
khs6j <66985912+khs6j@users.noreply.github.com>
2021-12-11 22:45:18 -0500

    fix keys

--- uvaguide/settings.py
+++ uvaguide/settings.py
@@ -33,10 +33,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY')
+SECRET_KEY = 'django-insecure-#2wcblq=2sect^r#wmrzkui2+#9n%r_q2j#r23io3r68+8d$i_'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['localhost','127.0.0.1','b-24-uvaguide.herokuapp.com', 'b-24-testing.herokuapp.com']
 
